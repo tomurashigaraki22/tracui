cf911f33d8ac8a7ea8d8bd312b272d88a0f90570
ikwerre-dev <investorhonour@gmail.com>
2025-05-30 14:17:30 +0100

    fix

--- src/lib/mysql.ts
+++ src/lib/mysql.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQL_DATABASE || 'sui_hackathon',
+  host: process.env.MYSQL_HOST || '131.153.147.50',
+  user: process.env.MYSQL_USER || 'cashload_hackathon',
+  password: process.env.MYSQL_PASSWORD || 'Trailer1234#',
+  database: process.env.MYSQL_DATABASE || 'cashload_hackathon',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
